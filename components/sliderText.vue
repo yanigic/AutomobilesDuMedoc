857eb62ed0a94c0b7b57a584e753004b9cced552
ya-sudo <y.tejeda@garage-italia.com>
2024-05-17 09:24:19 +0200

    save to safety

--- components/sliderText.vue
+++ components/sliderText.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div style="height: 80vh">
     <div class="container-SliderText">
       <div v-for="(image, index) in images" :key="index" class="mySlides">
         <div class="numbertext">{{ index + 1 }} / {{ images.length }}</div>
@@ -21,18 +21,14 @@
           />
         </svg>
       </a>
-      <div class="row-SliderText">
-        <div
-          class="column-SliderText"
-          style="max-height: 300px; overflow-y: auto"
-          ref="sliderContainer"
-        >
+
+      <div class="container-SliderText" ref="sliderContainer">
+        <div class="column-SliderText">
           <div
             v-for="(image, index) in images"
             :key="index"
             class="demo cursor"
-            @click="currentSlide(index + 1)"
-            :alt="'Slide ' + (index + 1)"
+            @click="plusSlides(index + 1 - slideIndex.value)"
             :class="{ activeSliderText: index + 1 === slideIndex }"
           >
             {{ image.name }}
@@ -40,29 +36,7 @@
         </div>
       </div>
 
-      <!-- <div class="row-SliderText">
-        <div
-          class="column-SliderText" 
-          v-for="(image, index) in images"
-          :key="index"
-        >
-          <div
-            class="demo cursor"
-            @click="currentSlide(index + 1)"
-            :alt="'Slide ' + (index + 1)"
-          >
-            {{ image.name }}
-          </div>
-          <!- <img
-            class="demo cursor"
-            :src="image.src"
-            style="width: 100%"
-            @click="currentSlide(index + 1)"
-            :alt="'Slide ' + (index + 1)"
-          /> --
-        </div>
-      </div> -->
-      <a class="next" @click="plusSlides(+1)"
+      <a class="next" @click="plusSlides(1)"
         ><svg
           xmlns="http://www.w3.org/2000/svg"
           width="60"
@@ -82,7 +56,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from "vue";
+import { ref, onMounted, nextTick, watch } from "vue";
 import "../assets/css/sliderText.css";
 import cardImage from "../assets/img/grid/image64.jpg";
 import cardImageOne from "../assets/img/grid/image73.jpg";
@@ -97,66 +71,52 @@ import cardImageNine from "../assets/img/grid/image70.jpg";
 
 // Immagini per lo slideshow
 const images = ref([
-  { src: cardImageFive, name: "4c Superleggera benz v12" },
+  { src: cardImageFive, name: "4c Superleggera" },
   { src: cardImageThree, name: "cherry countach" },
   { src: cardImageOne, name: "defender amaranth" },
   { src: cardImageOne, name: "bmw m3 e46" },
-  { src: cardImageFive, name: "4c Superleggera benz v12" },
+  { src: cardImageFive, name: "4c Superleggera" },
   { src: cardImageThree, name: "cherry countach" },
   { src: cardImageOne, name: "defender amaranth" },
   { src: cardImageOne, name: "bmw m3 e46" },
 ]);
 
 let slideIndex = ref(1);
+const sliderContainer = ref(null); // Definire la ref qui per usarla globalmente nel setup
 
 // Funzione per avanzare o retrocedere di una slide
 function plusSlides(n) {
-  showSlides((slideIndex.value += n));
-  scrollActiveSlideToTop();
+  const newIndex = slideIndex.value + n;
+  slideIndex.value =
+    newIndex < 1
+      ? images.value.length
+      : newIndex > images.value.length
+      ? 1
+      : newIndex;
+  centerActiveText();
 }
 
-// Funzione per visualizzare una slide specifica
-function currentSlide(n) {
-  slideIndex.value = n;
-  showSlides(n);
-  scrollActiveSlideToTop();
-}
-
-// Funzione per mostrare le slide
-function showSlides(n) {
-  let i;
-  const sliderContainer = ref(null);
-
-  if (sliderContainer.value) {
-    const slides = sliderContainer.value.getElementsByClassName(".demo");
-    const dots = sliderContainer.value.getElementsByClassName("demo");
-    const captionText = document.getElementById("caption");
-    if (n > slides.length) {
-      slideIndex.value = 1;
+// Sposta la logica di aggiornamento del centro qui
+function centerActiveText() {
+  nextTick(() => {
+    if (sliderContainer.value) {
+      const slides = sliderContainer.value.querySelectorAll(".demo");
+      const currentSlide = slides[slideIndex.value - 1];
+      if (currentSlide) {
+        // Calcola lo scorrimento necessario per centrare il testo
+        const scrollY =
+          currentSlide.offsetTop +
+          currentSlide.offsetHeight / 2 -
+          sliderContainer.value.offsetHeight / 2;
+        sliderContainer.value.scrollTop = scrollY;
+      }
     }
-    if (n < 1) {
-      slideIndex.value = slides.length;
-    }
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" activeSliderText", "");
-    }
-    slides[slideIndex.value - 1].style.display = "block";
-    dots[slideIndex.value - 1].className += " activeSliderText";
-    captionText.innerHTML = dots[slideIndex.value - 1].alt;
-  }
-}
-function scrollActiveSlideToTop() {
-  const sliderContainer = document.querySelector(".column-SliderText");
-  const activeSlide = sliderContainer.querySelector(".activeSliderText");
-  if (sliderContainer && activeSlide) {
-    sliderContainer.scrollTop = activeSlide.offsetTop;
-  }
+  });
 }
 
 onMounted(() => {
-  showSlides(slideIndex.value);
+  plusSlides(0); // Inizializza la vista
 });
+
+watch(slideIndex, centerActiveText); // Aggiorna la vista quando slideIndex cambia
 </script>

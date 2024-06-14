a6841417dde834528afae02c3b893f31840c622a
ya-sudo <y.tejeda@garage-italia.com>
2024-06-14 14:33:08 +0200

    deleted line: /Users/yanilettetejeda/Documents/Code/projects/adm/asset

--- components/heroHomePage.vue
+++ components/heroHomePage.vue
@@ -2,13 +2,6 @@
   <div class="box-video">
     <div class="container-img-animated container-img-animated-mobile">
       <img src="@/assets/img/IntroADM_Mobile_FrameFnale.png" alt="" />
-      <!--  <video v-else ref="videoElement" muted>
-        <source
-          src="/Users/yanilettetejeda/Documents/Code/projects/adm/assets/video/IntroADMMobile.mp4"
-          type="video/mp4"
-        />
-        Il tuo browser non supporta l'elemento video.
-      </video> -->
     </div>
     <div class="container-img-animated container-img-animated-desktop">
       <img
@@ -25,7 +18,6 @@
 </template>
 
 <script>
-import imgvidea from "/Users/yanilettetejeda/Documents/Code/projects/adm/assets/img/IntroADM_Desktop_FrameFnale.png";
 export default {
   data() {
     return {

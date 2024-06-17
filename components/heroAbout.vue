c190ceb457ed12037962798a998ce976054825a2
ya-sudo <y.tejeda@garage-italia.com>
2024-06-17 11:48:11 +0200

    save scroll delate

--- components/heroAbout.vue
+++ components/heroAbout.vue
@@ -6,7 +6,11 @@ import "../assets/css/heroAbout.css";
 
 <template>
   <div class="height-hero-about">
-    <div class="bg-img-about"><img :src="imgHero" alt="" /></div>
-    <div class="bg-img-about-mobile"><img :src="imgHeroMobile" alt="" /></div>
+    <div class="bg-img-about">
+      <img :src="imgHero" alt="" />
+    </div>
+    <div class="bg-img-about-mobile">
+      <img :src="imgHeroMobile" alt="" />
+    </div>
   </div>
 </template>

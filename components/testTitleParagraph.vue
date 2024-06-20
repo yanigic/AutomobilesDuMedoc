1126fefed61f38140f25997140fba12ec135f8bd
ya-sudo <y.tejeda@garage-italia.com>
2024-06-20 15:00:53 +0200

    save

---
+++ components/testTitleParagraph.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import "../assets/css/titleDescription.css";
+import "../assets/css/titleParagraph.css";
+import { ref, onMounted, onUnmounted } from "vue";
+
+const text = [
+  {
+    title: "Vision",
+    description:
+      "Our vision is to be the premier destination for exceptional and innovative vehicle customization. 'Unbound Custom Excellence' encapsulates our ethos of transcending traditional limits to provide a distinctive service experience, setting new standards in automotive craftsmanship and client personalization.",
+  },
+  {
+    title: "Mission",
+    description:
+      "Our mission is to deliver unparalleled customization services that fulfill our clients' desires for unique personal expression. We champion bespoke vehicle personalization with creative freedom and superior craftsmanship, ensuring every detail of the customization process enhances the individuality of each client's vehicle with uncompromised quality.",
+  },
+];
+
+const container = ref<HTMLElement | null>(null);
+
+const handleScroll = () => {
+  if (container.value) {
+    const imgSfondoParagraph = document.getElementById("imgSfondo-paragraph");
+    if (imgSfondoParagraph) {
+      let value = container.value.scrollTop;
+      imgSfondoParagraph.style.transform = `translateY(${value * -0.6}px)`;
+    }
+  }
+};
+
+onMounted(() => {
+  if (container.value) {
+    container.value.addEventListener("scroll", handleScroll);
+  }
+});
+
+onUnmounted(() => {
+  if (container.value) {
+    container.value.removeEventListener("scroll", handleScroll);
+  }
+});
+</script>
+
+<template>
+  <div ref="container" class="bg-paragraph-container">
+    <div class="bg-paragraph">
+      <div class="container-layout-paragraph">
+        <div
+          class="text-container-paragraph"
+          v-for="(item, index) in text"
+          :key="index"
+        >
+          <p class="titleParagraph">{{ item.title }}</p>
+          <p class="descriptionParagraph">{{ item.description }}</p>
+        </div>
+      </div>
+    </div>
+    <img
+      id="imgSfondo-paragraph"
+      src="../assets/img/Background2.png"
+      alt="Background Image"
+    />
+  </div>
+</template>
--- components/titleParagraph.vue
+++ components/titleParagraph.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<!-- <script setup lang="ts">
 import "../assets/css/titleDescription.css";
 import "../assets/css/titleParagraph.css";
 import { ref, onMounted, onUnmounted } from "vue";
@@ -24,6 +24,70 @@ const handleScroll = () => {
   if (imgSfondoParagraph) {
     let value = window.scrollY;
     imgSfondoParagraph.style.top = value * -0.8 + "px";
+    console.log("valore:", imgSfondoParagraph.style.top);
+  }
+};
+
+onMounted(() => {
+  window.addEventListener("scroll", handleScroll);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("scroll", handleScroll);
+});
+</script>
+
+<template>
+  <div class="bg-paragraph-container">
+    <div class="bg-paragraph">
+      <div class="container-layout-paragraph">
+        <div
+          class="text-container-paragraph"
+          v-for="(title, index) in text"
+          :key="index"
+        >
+          <p class="titleParagraph">{{ title.title }}</p>
+
+          <p class="descriptionParagraph">
+            {{ title.description }}
+          </p>
+        </div>
+      </div>
+    </div>
+    <img
+      id="imgSfondo-paragraph"
+      src="../assets/img/Background2.png"
+      alt="Background Image"
+    />
+  </div>
+</template>
+ -->
+<script setup lang="ts">
+import "../assets/css/titleDescription.css";
+import "../assets/css/titleParagraph.css";
+import { ref, onMounted, onUnmounted } from "vue";
+
+const text = [
+  {
+    title: "Vision",
+    description:
+      "Our vision is to be the premier destination for exceptional and innovative vehicle customization. 'Unbound Custom Excellence'  encapsulates our ethos of transcending traditional limits to provide a  distinctive service experience, setting new standards in automotive  craftsmanship and client personalization.",
+  },
+  {
+    title: "Mission",
+    description:
+      "Our mission is to deliver unparalleled customization services that  fulfill our clients' desires for unique personal expression. We champion  bespoke vehicle personalization with creative freedom and superior  craftsmanship, ensuring every detail of the customization process  enhances the individuality of each client's vehicle with uncompromised  quality.",
+  },
+];
+const isScrolled = ref(false);
+
+const handleScroll = () => {
+  isScrolled.value = window.scrollY > 0;
+  const imgSfondoParagraph = document.getElementById("imgSfondo-paragraph");
+
+  if (imgSfondoParagraph) {
+    let value = window.scrollY;
+    imgSfondoParagraph.style.top = value * -0.2 + "px";
     console.log("valore:", imgSfondoParagraph.style.top);
   }
 };

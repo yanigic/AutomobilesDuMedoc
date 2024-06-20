8dc16285a7dbd0adcb98418aba90ac20034d63a8
ya-sudo <y.tejeda@garage-italia.com>
2024-06-20 12:10:09 +0200

    change img hero about

--- components/lightgallery.vue
+++ components/lightgallery.vue
@@ -155,7 +155,7 @@ onBeforeUnmount(() => {
   transition: 0.3s;
 }
 
-.hover-shadow:hover {
+/* .hover-shadow:hover {
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-}
+} */
 </style>
